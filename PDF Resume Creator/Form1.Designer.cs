bc6a887d006eb0a0f222326db829c28d9bab3a73
Melody Meria <74742939+melodymers@users.noreply.github.com>
2022-07-12 18:35:36 +0800

    JSON file is generated and extensions are downloaded

--- PDF Resume Creator/Form1.Designer.cs
+++ PDF Resume Creator/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(718, 619);
+            this.ClientSize = new System.Drawing.Size(812, 619);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace PDF_Resume_Creator
 {
@@ -15,6 +15,11 @@ namespace PDF_Resume_Creator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
