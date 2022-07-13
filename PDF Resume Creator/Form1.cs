8256a99802fdf103f9a86b19956e9de150fcc6c2
Melody Meria <74742939+melodymers@users.noreply.github.com>
2022-07-13 12:09:53 +0800

    finished the input for json file

--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -12,9 +12,29 @@ namespace PDF_Resume_Creator
 {
     public partial class Form1 : Form
     {
+        private readonly string fileName = (@"C:\Users\Melody\source\repos\PDF Resume Creator\PDF Resume Creator\Resumecrtr.json");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class ResumeGen
+        {
+            public string name { get; set; }
+            public string label { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+            public string address { get; set; }
+            public string PostalCode { get; set; }
+            public string city { get; set; }
+            public string SocialMedia { get; set; }
+            public string username { get; set; }
+            public string URL { get; set; }
+            public string institution { get; set; }
+            public string course { get; set; }
+            public string language { get; set; }
+            public string interest { get; set; }
+            public string keywords { get; set; }
         }
 
         private void Form1_Load(object sender, EventArgs e)
